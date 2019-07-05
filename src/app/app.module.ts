54b414a54f9a665d8df7e420e988b50c48938b2e
Will Sun <will.sun@sap.com>
2019-07-05 11:07:29 +0800

    init inbox and today page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { BoardModule } from './board/board.module';
 import { AngularMaterialModule } from './core/angular-material.module';
+import { InboxComponent } from './page/inbox/inbox.component';
+import { TodayComponent } from './page/today/today.component';
 
 
 @NgModule({
   declarations: [
 	AppComponent,
+	InboxComponent,
+	TodayComponent,
   ],
   imports: [
 	BrowserModule,
