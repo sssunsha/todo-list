240d8c47bdfbabe862b0872192d510a7d982f604
Will Sun <will.sun@sap.com>
2019-07-05 16:06:08 +0800

    add detail page into app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
+
+	constructor(private service: AppService) {
+		
+	}
 }
