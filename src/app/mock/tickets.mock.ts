eeb9b221433d9c521770bec7a2fd9ef5546e0541
Will Sun <will.sun@sap.com>
2019-07-09 15:39:11 +0800

    add more ticket mock data

--- src/app/mock/tickets.mock.ts
+++ src/app/mock/tickets.mock.ts
@@ -49,6 +49,66 @@ export const mockTickets: Array<Ticket> =
 			progress: ETicktProgress.nearly_done,
 			inPages: [EPageState.inbox, EPageState.today],
 			records: ['records 1 ..', 'records 2 ...', 'records 3 ...'],
-		}
+		},
+		{
+			id: generateMd5Hash('4'),
+			summary: 'ticket 4 ... ... ... ...',
+			description: 'ticket 4 description ... ... ... ...',
+			priority: EPriority.medium,
+			createdAt: generateCreatedAt(),
+			modifiedAt: null,
+			ticketType: ETicketType.note,
+			// effort: ETicketEffort.half_hour,
+			progress: ETicktProgress.finished,
+			inPages: [EPageState.inbox, EPageState.future, EPageState.this_month, EPageState.this_week],
+			records: ['this the record for the first day I found', 'this the record for the second day I found', 'this the record for the thrid day I found']
+		},
+		{
+			id: generateMd5Hash('5'),
+			summary: 'ticket 5 ... ... ... ... ...',
+			description: 'ticket 5 description ... ... ... ...',
+			priority: EPriority.medium,
+			createdAt: generateCreatedAt(),
+			modifiedAt: null,
+			ticketType: ETicketType.note,
+			// effort: ETicketEffort.half_hour,
+			progress: ETicktProgress.half_done,
+			inPages: [EPageState.inbox, EPageState.life],
+			records: ['this the record for the first day I found', 'this the record for the second day I found', 'this the record for the thrid day I found']
+		},
+		{
+			id: generateMd5Hash('6'),
+			summary: 'ticket 6 ... ... ... ... ...',
+			description: 'ticket 6 description ... ... ... ...',
+			priority: EPriority.medium,
+			createdAt: generateCreatedAt(),
+			modifiedAt: null,
+			ticketType: ETicketType.note,
+			// effort: ETicketEffort.half_hour,
+			progress: ETicktProgress.half_done,
+			inPages: [EPageState.inbox, EPageState.work, EPageState.others],
+			records: ['this the record for the first day I found', 'this the record for the second day I found', 'this the record for the thrid day I found']
+		},
+		{
+			id: generateMd5Hash('7'),
+			summary: 'ticket 3, this is a test ticket with a very long long text,  I want to use this \
+			long long text to test the table support for long long text, so let me have a look how is\
+			the performance, hey hey. ticket 3, this is a test ticket with a very long long text,  I want to use this \
+			long long text to test the table support for long long text, so let me have a look how is\
+			the performance, hey hey.',
+			description: 'ticket 3, this is a test ticket with a very long long text,  I want to use this \
+			long long text to test the table support for long long text, so let me have a look how is\
+			the performance, hey hey. ticket 3, this is a test ticket with a very long long text,  I want to use this \
+			long long text to test the table support for long long text, so let me have a look how is\
+			the performance, hey hey.',
+			priority: EPriority.low,
+			createdAt: generateCreatedAt(),
+			modifiedAt: null,
+			ticketType: ETicketType.reminder,
+			effort: ETicketEffort.one_month,
+			progress: ETicktProgress.nearly_done,
+			inPages: [EPageState.inbox, EPageState.today],
+			records: ['records 1 ..', 'records 2 ...', 'records 3 ...'],
+		},
 		
 	];
