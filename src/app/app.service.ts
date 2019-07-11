af2243269cf5fafdb7f8c5223c0c8b437e0e4ed6
Will Sun <will.sun@sap.com>
2019-07-11 18:06:42 +0800

    add draft cos implementation codes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { EPageState, Ticket } from './app.model';
 import { mockTickets } from './mock/tickets.mock';
+// import * as COS from 'cos-nodejs-sdk-v5';
+import * as COS from 'cos-js-sdk-v5';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ export class AppService {
   
   // store tickets data here
   tickets: Array<Ticket>;
+  cos: any;
 
   constructor() {
 	  this.currentPage = EPageState.today;
@@ -18,5 +21,16 @@ export class AppService {
    }
 
    initClient(): void {
+	   this.cos = new COS({
+		   SecretId: 'AKIDDJEzZ0xJGvsssiRp7HRV4EIBtRo1p5tZ',
+		   SecretKey: 'd4JMEwSr690vgFdppspdKqAJH4f4FLMC'
+	   });
+	   this.cos.getBucket({
+		Bucket: 'dashboard-1255953405',
+		Region: 'ap-chengdu',
+		Prefix: '/',
+	}, function (err, data) {
+		console.log(err || data.Contents);
+	});
 	}
 }
