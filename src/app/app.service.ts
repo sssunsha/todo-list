6a02b75c3db92d0e9a3e175b1c5df1196f62f1cd
Will Sun <will.sun@sap.com>
2019-07-11 17:45:50 +0800

    remove evernote relative codes

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@angular/core';
 import { EPageState, Ticket } from './app.model';
 import { mockTickets } from './mock/tickets.mock';
 
-import * as Evernote from 'evernote';
-
-export const callbackUrl = "http://localhost:4200/oauth_callback";
-
 @Injectable({
   providedIn: 'root'
 })
@@ -15,10 +11,6 @@ export class AppService {
   // store tickets data here
   tickets: Array<Ticket>;
 
-  evernoteClient: any;
-  oauthToken: string;
-  oauthTokenSecret: string;
-
   constructor() {
 	  this.currentPage = EPageState.today;
 	  this.tickets = mockTickets;
@@ -26,15 +18,5 @@ export class AppService {
    }
 
    initClient(): void {
-	   this.evernoteClient =  new Evernote.Client({sandbox: false, token: 'S=s31:U=6d0aeb:E=16c040e783b:\
-	   C=16be001f0c0:P=1cd:A=en-devtoken:V=2:H=c4e0572c15d3c83875ed799e0364dd4a'});
-
-	   const noteStore = this.evernoteClient.getNoteStore();
-	   noteStore.listNotebooks((notebooks => {
-		   notebooks.forEach(notebook => {
-			   console.log('notebook: ' + notebook.name);
-		   });
-	   }));
-
 	}
 }
